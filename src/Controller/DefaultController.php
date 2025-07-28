bc15dd63c7079550a1e88fc09e304109d464c186
lpcarvalho97 <lucasadmcarvalho@outlook.com>
2025-07-28 19:48:58 -0300

    Primeiro Commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController
+{   #[Route('/', name: 'app_index')]
+
+    public function index(Request $request): Response
+    {
+        $resp = new Response();
+        $resp->setContent(json_encode(
+            [
+                "recebido" => $request->get('nome'),
+                "ip" => $request->getClientIp()
+            ]
+        ));
+        $resp->setStatusCode(200);
+
+        return $resp;
+    }
+}
