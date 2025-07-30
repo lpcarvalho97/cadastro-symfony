088c0315c3d43e9585b89319497c67d2a78ce2b7
lpcarvalho97 <lucasadmcarvalho@outlook.com>
2025-07-29 21:06:01 -0300

    Penúltima aula

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -5,6 +5,11 @@
     use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Request;
+    use App\Entity\Usuario;
+    use Doctrine\Persistence\ManagerRegistry;
+    use Doctrine\ORM\EntityManagerInterface;
+    use Symfony\Component\HttpFoundation\JsonResponse;
 
     #[Route('/', name: 'web_usuario_')]
     class UsuarioController extends AbstractController
@@ -16,8 +21,34 @@
         }
 
         #[Route('/salvar', methods:['POST'], name: 'salvar')]
-        public function salvar(): Response
+        public function salvar(Request $request, EntityManagerInterface $entityManager): Response
         {
-            return new Response("implementar gravação ao banco de dados");
+
+            $data = $request->request->all();
+
+            $usuario = new Usuario();
+
+            $usuario->setNome($data['nome']);
+            $usuario->setEmail($data['email']);
+
+            $entityManager->persist($usuario);
+            $entityManager->flush();
+
+            //$doctrine = $this->getDoctrine()->getManager();
+            //$doctrine->persist($usuario);
+            //$doctrine->flush();
+
+            //if( $usuario->getId() )
+            if( $doctrine->contains($usuario) )
+            {
+                return $this->render("usuario/sucesso.html.twig", [
+                    "fulano" => $data['nome']
+                ]);
+            } else {
+                return $this->render("usuario/erro.html.twig", [
+                    "fulano" => $data['nome']
+                ]);
+            }
+
         }
     }
